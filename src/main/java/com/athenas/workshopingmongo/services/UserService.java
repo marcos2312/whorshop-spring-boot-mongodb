0341db69bfa89544d460d04b84675fe7e161bd9f
marcos2312 <marcos.rcomercial@gmail.com>
2023-05-28 11:43:27 -0300

    Obtento um usuario por id

--- src/main/java/com/athenas/workshopingmongo/services/UserService.java
+++ src/main/java/com/athenas/workshopingmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.athenas.workshopingmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.athenas.workshopingmongo.domain.User;
 import com.athenas.workshopingmongo.repository.UserRepository;
+import com.athenas.workshopingmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,10 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	}
+
